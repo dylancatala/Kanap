10a708363b569a61cf4b88a9559f43f4ec55b4f2
Dylan Catala <dylan.catala@proton.me>
2023-01-15 19:17:38 +0100

    Adding products ID to every link cards w/ for loop

--- front/js/script.js
+++ front/js/script.js
@@ -1,35 +1,40 @@
+// Fetch data from API
+const dataBrowse = async () => {
+  let url = `http://localhost:3000/api/products`;
+  let data = await fetch(url);
+  let response = await data.json();
 
-const dataBrowse = async() => {
-let url = `http://localhost:3000/api/products`;
-let data = await fetch(url);
-let response = await data.json();
+  for (i = 0; i < response.length; i++) {
+    const products = response[i];
+    // AHREF
+    const aLink = document.createElement("a");
+    aLink.classList.add("productPage");
+    aLink.setAttribute("href", `http://127.0.0.1:5500/front/html/product.html?id=${products._id}`);
+    // BALISE ARTICLE
+    const articleTag = document.createElement("article");
+    // Image
+    const imageElement = document.createElement("img");
+    imageElement.src = products.imageUrl;
+    //Nom Produit
+    const productName = document.createElement("h3");
+    productName.innerHTML = products.name;
+    //Description produit
+    const productDescription = document.createElement("p");
+    productDescription.innerHTML = products.description;
+
+    // Adding elements to my ID
+    const addElements = document.querySelector("#items");
+    addElements.appendChild(aLink);
+    aLink.appendChild(articleTag);
+    articleTag.appendChild(imageElement);
+    articleTag.appendChild(productName);
+    articleTag.appendChild(productDescription);
+  }
 
 
-for(i = 0; i < response.length; i++) {
-  const products = response[i];
-  // AHREF
-  const aLink = document.createElement("a");
-  // BALISE ARTICLE
-  const articleTag = document.createElement("article");
-  // Image
-  const imageElement = document.createElement("img");
-  imageElement.src = products.imageUrl;
-  //Nom Produit
-  const productName = document.createElement("h3");
-  productName.innerHTML = products.name;
-  //Description produit
-  const productDescription = document.createElement("p");
-  productDescription.innerHTML = products.description;
 
-  // Adding elements to my ID
-  const addElements = document.querySelector("#items");
-  addElements.appendChild(aLink);
-  aLink.appendChild(articleTag);
-  articleTag.appendChild(imageElement);
-  articleTag.appendChild(productName);
-  articleTag.appendChild(productDescription);
+
+
 
 }
-}
-
 dataBrowse();
